1934429dace11678883d45d2c7dc433491bb1c83
hiroaki kaneda <emboss369@gmail.com>
2020-06-05 19:09:16 +0900

    03-03

---
+++ Todo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Todo
+//
+//  Created by user.name on 2020/05/11.
+//  Copyright © 2020 example.com. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
