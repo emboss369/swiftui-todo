c751b2392826c6b453ae6da0f699fe565d513a0b
hiroaki kaneda <emboss369@gmail.com>
2020-06-05 19:31:11 +0900

    16-03

--- Todo/TodoList.swift
+++ Todo/TodoList.swift
@@ -17,20 +17,38 @@ struct TodoList: View {
         animation: .default)
     var todoList: FetchedResults<TodoEntity>
 
+    @Environment(\.managedObjectContext) var viewContext
+    
+    fileprivate func deleteTodo(at offsets: IndexSet) {
+        for index in offsets {
+            let entity = todoList[index]
+            viewContext.delete(entity)
+        }
+        do {
+            try viewContext.save()
+        } catch {
+            print("Delete Error. \(offsets)")
+        }
+    }
     
     let category: TodoEntity.Category
     
     var body: some View {
-        VStack {
-            List {
-                ForEach(todoList) { todo in
-                    if todo.category == self.category.rawValue {
-                        TodoDetailRow(todo: todo, hideIcon: true)
-                    }
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(todoList) { todo in
+                        if todo.category == self.category.rawValue {
+                            NavigationLink(destination: EditTask(todo: todo)) {
+                                TodoDetailRow(todo: todo, hideIcon: true)
+                            }
+                        }
+                    }.onDelete(perform: deleteTodo)
                 }
-            }
-            QuickNewTask(category: category)
-                .padding()
+                QuickNewTask(category: category)
+                    .padding()
+            }.navigationBarTitle(category.toString())
+            .navigationBarItems(trailing: EditButton())
         }
     }
 }
